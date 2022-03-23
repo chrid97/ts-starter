f84899477daddfae093eb2ede3a2b91bfc7bab26
chrid97 <cgoncal12@gmail.com>
2022-03-22 20:20:58 -0400

    Change ignoreContent string to an array of strings
    In the newly written .ignore files there were spaces infront of the
    folder names because of the way I stored the string. This made it so
    the folders weren't recognized. This change should get rid of the issue.

--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,9 @@
 
 import readline from "readline";
 import fs from "fs";
-import { exec, execSync } from "child_process";
+import { execSync } from "child_process";
 
-const ignoreContent = `
-  node_modules
-  dist
-`;
-
+const ignoreContent = ["build", "node_modules"].join("\n");
 const prettierContent = `
   {
     "semi": true,
