f8a3270d10ea212816500840df6794a61a067fd3
chrid97 <cgoncal12@gmail.com>
2023-10-22 10:19:32 -0400

    Add functionality to install prettier/update and create a prettier config

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use std::fs::File;
-use std::io::prelude::*;
+use std::io::{self, prelude::*};
+use std::path::Path;
+use std::process::Command;
 
 fn main() {
     let _ = install_or_update_prettier();
 }
 
 fn install_or_update_prettier() -> std::io::Result<()> {
-    // check if prettier is installed in package.json
-    // then check if prettier file exists
-    // const INSTALL_PRETTIER_COMMAND: &str = "npm install --save-dev --save-exact prettier";
     const PRETTIER_FILE_CONFIG: &str = r#"{
     "semi": true,
     "singleQuote": true,
@@ -16,8 +15,20 @@ fn install_or_update_prettier() -> std::io::Result<()> {
     "arrowParens": "always",
     "tabWidth": 4
 }"#;
+    let output = Command::new("npm")
+        .arg("install")
+        .arg("--save-dev")
+        .arg("--save-exact")
+        .arg("prettier")
+        .output()
+        .expect("Didn't work");
+    println!("status: {}", output.status);
+    io::stdout().write_all(&output.stdout).unwrap();
+    io::stderr().write_all(&output.stderr).unwrap();
+    println!("{}", Path::new(".prettierrc").exists());
     let mut file = File::create(".prettierrc").expect("uh oh");
-    file.write_all(PRETTIER_FILE_CONFIG.as_bytes()).expect("bzzt");
+    file.write_all(PRETTIER_FILE_CONFIG.as_bytes())
+        .expect("bzzt");
     Ok(())
 }
 
