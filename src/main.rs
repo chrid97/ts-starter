62335eef515d80b5c30eba637ebc6aefc2503778
chrid97 <cgoncal12@gmail.com>
2023-10-22 12:22:08 -0400

    Add function to update tsconfig.json

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,36 @@
-use std::fs::File;
+use std::fs::{File, self};
 use std::io::{self, prelude::*};
 use std::path::Path;
 use std::process::Command;
+use serde::{Serialize, Deserialize};
+
+#[allow(non_snake_case)]
+#[derive(Deserialize, Serialize, Debug)]
+struct TsConfig {
+    compilerOptions: Vec<String>,
+}
 
 fn main() {
-    let _ = install_or_update_prettier();
+    update_tsconfig();
+    // let _ = install_or_update_prettier();
+}
+
+fn update_tsconfig() {
+    const TSCONFIG_STRICT_RULES: &str = r#"{
+    "strict": true,
+    "strictNullChecks": true,
+    "noImplicitAny": true,
+    "noUnusedLocals": true,
+    "noUnusedParameters": true,
+    "noFallthroughCasesInSwitch": true
+}"#;
+    let tsconfig_json = fs::read_to_string("tsconfig.json").expect("unable to read json");
+    let json = serde_json::from_str::<TsConfig>(&tsconfig_json).expect("wrong");
+    println!("{:#?}", json);
+    // let mut file = File::create("tsconfig.json").expect("uh oh");
+    // file.write_all(TSCONFIG_STRICT_RULES.as_bytes())
+    //     .expect("bzzt");
+
 }
 
 fn install_or_update_prettier() -> std::io::Result<()> {
@@ -25,7 +51,7 @@ fn install_or_update_prettier() -> std::io::Result<()> {
     println!("status: {}", output.status);
     io::stdout().write_all(&output.stdout).unwrap();
     io::stderr().write_all(&output.stderr).unwrap();
-    println!("{}", Path::new(".prettierrc").exists());
+    // println!("{}", Path::new(".prettierrc").exists());
     let mut file = File::create(".prettierrc").expect("uh oh");
     file.write_all(PRETTIER_FILE_CONFIG.as_bytes())
         .expect("bzzt");
