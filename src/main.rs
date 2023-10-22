eae9f7b3ff0e00fc6b761d0a0e30fe2c0fe94541
chrid97 <cgoncal12@gmail.com>
2023-10-22 12:22:08 -0400

    Add function to update tsconfig.json

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,73 @@
-use std::fs::File;
+use json_comments::StripComments;
+use serde::{Deserialize, Serialize};
+use std::collections::HashMap;
+use std::fs::{self, File};
 use std::io::{self, prelude::*};
-use std::path::Path;
 use std::process::Command;
 
-fn main() {
-    let _ = install_or_update_prettier();
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(rename_all="camelCase")]
+struct TsConfig {
+    compiler_options: HashMap<String, CompilerOptions>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    include: Option<Vec<String>>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    exclude: Option<Vec<String>>,
 }
 
-fn install_or_update_prettier() -> std::io::Result<()> {
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(untagged)]
+enum CompilerOptions {
+    HashMap(HashMap<String, Vec<String>>),
+    String(String),
+    Boolean(bool),
+    Vec(Vec<HashMapOrString>),
+}
+
+#[derive(Deserialize, Serialize, Debug)]
+#[serde(untagged)]
+enum HashMapOrString {
+    String(String),
+    HashMap(HashMap<String, String>),
+}
+
+fn main() {
+    update_tsconfig();
+    install_or_update_prettier();
+}
+
+fn update_tsconfig() {
+    let tsconfig_json = fs::read_to_string("tsconfig.json").expect("Failed to read tsconfig.json");
+    let mut buf = String::new();
+    StripComments::new(tsconfig_json.as_bytes())
+        .read_to_string(&mut buf)
+        .expect("Unable to strip comments from tsconfig.json");
+    let mut json =
+        serde_json::from_str::<TsConfig>(&buf).expect("Failed to deserialize tsconfig.json");
+    let _ = &json
+        .compiler_options
+        .insert(String::from("strict"), CompilerOptions::Boolean(true));
+    let _ = &json.compiler_options.insert(
+        String::from("strictNullChecks"),
+        CompilerOptions::Boolean(true),
+    );
+    let _ = &json.compiler_options.insert(
+        String::from("noUnusedLocals"),
+        CompilerOptions::Boolean(true),
+    );
+    let _ = &json.compiler_options.insert(
+        String::from("noUnusedParameters"),
+        CompilerOptions::Boolean(true),
+    );
+    let _ = &json.compiler_options.insert(
+        String::from("noFallthroughCasesInSwitch"),
+        CompilerOptions::Boolean(true),
+    );
+    let json = serde_json::to_string_pretty(&json).expect("Failed to serialize tsconfig.json");
+    fs::write("tsconfig.json", json).expect("Failed to write to tsconfig.json");
+}
+
+fn install_or_update_prettier() {
     const PRETTIER_FILE_CONFIG: &str = r#"{
     "semi": true,
     "singleQuote": true,
@@ -25,14 +85,12 @@ fn install_or_update_prettier() -> std::io::Result<()> {
     println!("status: {}", output.status);
     io::stdout().write_all(&output.stdout).unwrap();
     io::stderr().write_all(&output.stderr).unwrap();
-    println!("{}", Path::new(".prettierrc").exists());
     let mut file = File::create(".prettierrc").expect("uh oh");
     file.write_all(PRETTIER_FILE_CONFIG.as_bytes())
         .expect("bzzt");
-    Ok(())
 }
 
-// update/generate tsconfig and eslint
+// and eslint
 // create next projects from here
 // when generating a next project remove the boilerplate
 // and ammend the original commit to say Initial Commit
